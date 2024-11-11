4252809aca57fc81e9d6d3b73d16adb0831e0b3e
Sambhav jain <fingma624@gmail.com>
2024-11-11 21:39:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App text-2xl text-cyan-500">
+   namskar bundelkhand
+    </div>
+  );
+}
+
+export default App;
